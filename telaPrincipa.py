5d598690346d967e2815af9315ec524f454ae127
reinaldo22 <vargas.reynaldo22@gmail.com>
2017-06-16 15:13:44 -0400

    atualizando

--- telaPrincipa.py
+++ telaPrincipa.py
@@ -1,69 +1,73 @@
 import pygame
+from pygame.locals import *
+from sys import exit
+from random import randrange
 import random
-
 pygame.init()
+
 BLACK = [0,0,0]
 WHITE = [255,255,255]
-# Defina a altura e a largura da tela
-tamanho = [400,400]
+tamanho = [620,498]
 
-screen = pygame.display.set_mode(tamanho)
-pygame.display.set_caption("Tela Principal")
-#cria duas imagens
-backgroun_image1 = pygame.image.load("estrela.png").convert()
-backgroun_image2 = pygame.image.load("ship.png").convert()
+def tela_flocos():
+    screen = pygame.display.set_mode(tamanho)
+    pygame.display.set_caption("Tela Principal")
+    background_filename = 'estrela.jpg'
+    background = pygame.image.load(background_filename).convert()
+    ship_filename = 'ship.png'
+    ship = pygame.image.load(ship_filename).convert_alpha()
+    ship_position = [randrange(100), randrange(200)]
 
-# Criar uma matriz vazia
-matriz = []
+    clock = pygame.time.Clock()
+    while True:
+        speed = {
+            'x': 0,
+            'y': 0
+        }
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_UP]:
+            speed['y'] = -5
+        elif pressed_keys[K_DOWN]:
+            speed['y'] = 5
+        if pressed_keys[K_LEFT]:
+            speed['x'] = -5
+        elif pressed_keys[K_RIGHT]:
+            speed['x'] = 5
+        screen.blit(background, (0, 0))
+        ship_position[0] += speed['x']
+        ship_position[1] += speed['y']
+        screen.blit(ship, ship_position)
+        pygame.display.update()
+        time_passed = clock.tick(100)
 
-# Loop 50 vezes e adicione
-for i in range(50):
-    x = random.randrange(0,600)
-    y = random.randrange(0,600)
-    matriz.append([x,y])
-clock = pygame.time.Clock()
-
-pressionar = False
-while not pressionar:
-    for event in pygame.event.get():#o usuario pressionou algo
-        if event.type == pygame.QUIT:
-            pressionar = True  # Indique que estamos preparados para que saia deste loop
-
-    # Defina o fundo da tela
-    screen.fill(BLACK)
-
-    screen.blit(backgroun_image1, [0, 0])
-    screen.blit(backgroun_image2, [0, 0])
-
-
-    # Processe cada floco de neve na lista
-    for i in range(len(matriz)):
-        # Desenhe o floco de neve
-        pygame.draw.circle(screen, WHITE, matriz[i], 2)
-
-        # Mova o floco de neve para baixo um pixel
-        matriz[i][1] += 1
-
-        # Se o floco de neve se deslocou da parte inferior da tela
-        if matriz[i][1] > 600:
-
-            # Reinicie-o apenas acima do topo
-            y = random.randrange(-50, -10)
-            matriz[i][1] = y
-
-
-            x = random.randrange(0, 400)
-            matriz[i][0] = x
-
-    #atualize a tela com o que foi desenhado.
-    pygame.display.flip()
-    clock.tick(30)
-#saindo do processo
+        matriz = []
+        for i in range(50):
+            x = random.randrange(0, 600)
+            y = random.randrange(0, 750)
+            matriz.append([x, y])
+            clock = pygame.time.Clock()
+            for i in range(len(matriz)):
+               # Desenhe o floco de neve
+               pygame.draw.circle(screen, WHITE, matriz[i], 1)
+               matriz[i][1] += 1
+               if matriz[i][1] > 500:
+                   y = random.randrange(-50, -20)
+                   matriz[i][1] = y
+                   x = random.randrange(0, 400)
+                   matriz[i][0] = x
+        pygame.display.flip()
+        clock.tick(30)
 pygame.quit()
 
 def desenho (screen, x,y):
     pygame.draw.ellipse(screen,WHITE,[35+x,y,25,25])
-    pygame.draw.ellipse(screen, WHITE, [23 + x, 20 + y, 50, 50])
+    pygame.draw.ellipse(screen, WHITE, [23+ x, 19 + y, 50, 50])
     pygame.draw.ellipse(screen, WHITE, [x, 65 + y, 100, 100])
 
+
+tela_flocos()
 desenho()
+
